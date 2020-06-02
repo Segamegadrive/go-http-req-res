0e58fadb03157bf2875b48e733046d179000ec8a
unknown <segamegadrive153@hotmail.com>
2020-06-02 15:07:02 +0100

    DefaultClient required for headers

--- main.go
+++ main.go
@@ -7,11 +7,15 @@ import (
 )
 
 func main() {
-	response, err := http.Get("http://aio-2521:8041/v1/metric/")
+	url := "http://aio-2521:8041/v1/metric/"
+	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
-		fmt.Printf("The HTTP request failed %s\n", err)
+		fmt.Printf("HTTP error %s\n", err)
 	} else {
-		data, _ := ioutil.ReadAll(response.Body)
+		req.Header.Add("x-auth-token", "gAAAAABe1laWSJy5aAjQZ5HgFCSapt1aOZAwDBAspPWTrb7SnzuK0AOOHUYqxSZFQd2VWIMWCrWYPUYGM-zLbvuXO30pq77nFPDbI4TFLFemwDao0fNLdCNQsSa52nPL5ZhyMwopArp0A3GS1mP0fHxNvo2Wle94kK5eRTafB_JkJ2ZcXHt8Pww")
+		res, _ := http.DefaultClient.Do(req)
+		data, _ := ioutil.ReadAll(res.Body)
 		fmt.Println(string(data))
 	}
+
 }
